1a9a952eb0ddd4db01618e40089fc218685fc29a
khushig222 <khushi.g@intimetec.com>
2025-01-16 21:30:27 +0530

    made changes in day-1

--- week-1/day1.ps1
+++ week-1/day1.ps1
@@ -1,2 +1,12 @@
-this is the link of my github repository.
-https://github.com/khushig222/Powershell.git
+param (
+    $ExecutionPolicy = "Unrestricted"
+)
+
+# Check if the execution policy is set to Unrestricted
+if ($ExecutionPolicy -eq "Unrestricted") {
+    # Set the execution policy to Unrestricted for the current session
+    Set-ExecutionPolicy -ExecutionPolicy $ExecutionPolicy -Scope Process -Force
+    Write-Host "Execution Policy set to $ExecutionPolicy for this session."
+} else {
+    Write-Host "The provided execution policy is not $ExecutionPolicy . Please specify $ExecutionPolicy as the execution policy."
+}
